c592690a0aa0930e3b828f9aa57877da33726ead
sparkling0 <908585531@qq.com>
2024-10-19 21:57:38 +0800

    slove macros

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,8 +5,9 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+
+#[macro_use]  // 添加这个属性让宏在模块外部可见
 mod macros {
     macro_rules! my_macro {
         () => {
